c11918a3d1c5243624dd84b84952c41b062bc5b1
Vimla Dewasi <66508837+vimladewasi@users.noreply.github.com>
2020-08-07 19:44:27 +0530

    Update Game.js

--- Game.js
+++ Game.js
@@ -30,13 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
-    car1.addImage("car1",car1_img);
+    //car1.addImage("car1",car1_img);
     car2 = createSprite(300,200);
-    car2.addImage("car2",car2_img);
+    //car2.addImage("car2",car2_img);
     car3 = createSprite(500,200);
-    car3.addImage("car3",car3_img);
+ //   car3.addImage("car3",car3_img);
     car4 = createSprite(700,200);
-    car4.addImage("car4",car4_img);
+    //car4.addImage("car4",car4_img);
     cars = [car1, car2, car3, car4];
   }
 
