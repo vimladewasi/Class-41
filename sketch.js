b7dfd7de2faef74a212bdc1c4cce4fd1e3c83d9d
Vimla Dewasi <66508837+vimladewasi@users.noreply.github.com>
2020-08-07 19:43:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,12 +13,13 @@ var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("track.jpg");
+  /*track = loadImage("track.jpg");
   car1_img = loadImage("car1.png");
   car2_img = loadImage("car2.png");
   car3_img = loadImage("car3.png");
   car4_img = loadImage("car4.png");
   ground = loadImage("ground.png");
+  */
 }
 
 function setup(){
